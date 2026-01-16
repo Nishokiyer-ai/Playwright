c2a12c017bf972b8d7da6d5acd90e8cb52d56a83
NISHOK IYER <nishokiyer@gmx.com>
2026-01-16 13:38:22 -0400

    Add files via upload
    Premetive data types

---
+++ Day1_JavaScript_Basics/premitiveDataType.js
@@ -0,0 +1,36 @@
+// Premitive Data types in JavaScript
+
+var firstName = 'John'; // String
+console.log(firstName);
+console.log(typeof firstName);
+
+var companyName = "Manaran Company"; // String
+console.log(companyName);
+console.log(typeof companyName);
+
+var mobileNumber = 9876543210; // Number
+console.log(mobileNumber);
+console.log(typeof mobileNumber);
+
+var isAutomation = true; // Boolean
+console.log(isAutomation);
+console.log(typeof isAutomation);
+
+var hasPlaywright
+console.log(typeof hasPlaywright); // Undefined
+console.log(hasPlaywright);
+
+var accountNumber = 45435435345345433453n;
+console.log(accountNumber); // BigInt
+console.log(typeof accountNumber);
+
+var abc = null; // Null
+console.log(abc);
+console.log(typeof abc); // Note: typeof null returns 'object' due to a historical bug in JavaScript
+
+var prism = `This is a symbol`;
+console.log(prism); // Symbol
+console.log(typeof prism);
+
+var xyz;
+console.log(xyz);
